4451afcbb24c6911790185223bf75c60585de76d
HittmanA <hittmana@comcast.net>
2016-02-12 09:41:04 -0500

    Plugin stuff
    Stuff and files for the plugin

---
+++ src/Hittmana/SAM/MainClass.php
@@ -0,0 +1,36 @@
+<?php
+namespace Hittmana\SAM;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\event\Listener;
+
+class MainClass extends PluginBase implements Listener{
+    public function onEnable()
+    {
+        $this->getLogger()->info("[SAM] SAM v0.0.1 enabled");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+    
+    public function onDisable()
+    {
+        $this->getLogger()->info("[SAM] SAM v0.0.1 disabled");
+    }
+
+    function onJoin(PlayerJoinEvent $event)
+     {
+    $name = $event->getPlayer()->getDisplayName();    
+    if ($registered = FALSE){
+        //$registered is whether or not the player is registered. Set it to retrieve the players info from the $url
+        $name->sendMessage("[SAM] Hello " . $name . " this server use SAM by Edwardthedog2. You must register at " . $url . " to play");  
+     //$url is the url of the users website
+    }
+    if ($registered = TRUE){
+        //$registered is whether or not the player is registered. Set it to retrieve the players info from the $url
+        $name->sendMessage("[SAM] Hello" . $name . " To play you must login with /login (your password)");  
+     //$url is the url of the users website
+    }
+  }
+}
